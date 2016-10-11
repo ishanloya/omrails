7a45a98c9d440839c54470fbad23eb3eb2a4f2f4
Ishan Loya <ishanloya@gmail.com>
2016-10-11 09:47:11 +0530

    Create home, about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact_us'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
