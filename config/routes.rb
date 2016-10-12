e052144fc9248d3585588eb1c71b30175bc5e626
Ishan Loya <ishanloya@gmail.com>
2016-10-12 21:35:27 +0530

    Add content, sticky footer and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'services' => 'pages#services'
+  get 'projects' => 'pages#projects'
+  get 'recognition' => 'pages#recognition'
   get 'contact' => 'pages#contact_us'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
