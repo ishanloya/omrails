829e7179653d8d3f0b5e2b50ce8724ff84a09e65
Ishan Loya <ishanloya@gmail.com>
2016-11-05 22:37:56 +0530

    Add item validations, require user, titla and either url or text

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :title, presence: true
+  validates :url, presence: true, unless: Proc.new { |a| a.text.present? }
+  validates :text, presence: true, unless: Proc.new { |a| a.url.present? }
 end
