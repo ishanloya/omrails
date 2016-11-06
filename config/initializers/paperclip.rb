5795a6e4576a46be0e2e702e44bf9e927902f49e
Ishan Loya <ishanloya@gmail.com>
2016-11-07 00:21:50 +0530

    Fix Amazon S3 url in paperclip defaults

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,6 +4,6 @@ Rails.application.config.before_initialize do
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
 
-Paperclip::Attachment.default_options[:s3_host_name] = 'us-standard.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_region] = 'us-west-2'
 Paperclip::Attachment.default_options[:bucket] = 'heroku-rails'
