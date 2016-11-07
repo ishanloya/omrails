5972704ff08ac7cc258bc8a2b5c66560795deeac
Ishan Loya <ishanloya@gmail.com>
2016-11-07 07:17:48 +0530

    Change item index layout, add likes and dislikes with bootstrap pills

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,12 +4,12 @@ class VotesController < ApplicationController
   def create
     item = Item.find(params[:id])
     item.liked_by current_user
-    redirect_to items_path, notice: "You successfully voted for an item"
+    redirect_to items_path, notice: "Thanks for liking!"
   end
 
   def destroy
     item = Item.find(params[:id])
     item.unliked_by current_user
-    redirect_to items_path, notice: "You successfully unvoted for an item"
+    redirect_to items_path, notice: "You successfully unliked an item"
   end
 end
