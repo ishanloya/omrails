aa114e0234375dc3089a0a8f2b1d994f3b57f27b
Ishan Loya <ishanloya@gmail.com>
2016-11-21 11:18:42 +0530

    Fix pdf footer alignment and position

--- app/models/ward_pdf.rb
+++ app/models/ward_pdf.rb
@@ -8,7 +8,20 @@ class WardPdf < Prawn::Document
 		ward_admin
 		labour_strength
 		logo
+		boxy
 		footer
+	end
+
+	
+	# stroke_axis
+	# stroke_circle [0, 0], 10
+	def boxy
+		# stroke_axis
+		# stroke_circle [0, 0], 10
+		bounding_box([100, 300], :width => 300, :height => 200) do
+		 stroke_bounds
+		 stroke_circle [0, 0], 10
+		end
 	end
 
 	def logo
@@ -18,13 +31,12 @@ class WardPdf < Prawn::Document
 
 	def ward_intro
 		text "<font size='30'><color rgb='000000'><b>Ward #{@ward.ward_number}</b></color></font>  Zone #{@ward.zone}",
-		size: 20, color: '818a91', :inline_format => true  
+		size: 20, color: '818a91', inline_format: :true  
 
 		text "#{@ward.ward_name}"
 	end
 
 	def ward_admin
-		move_down 20
 		text_box "Ward officer", size: 16, style: :bold, color: '818a91', at: [0, 680]
 		text_box "#{@ward.ward_officer}\nPh: #{@ward.ward_officer_number}", at: [0, 660]
 		
@@ -37,40 +49,7 @@ class WardPdf < Prawn::Document
 		text_box "Jawan", size: 16, style: :bold, color: '818a91', at: [250, 580+30]
 		text_box "#{@ward.jawan}\nPh: #{@ward.jawan_number}", at: [250, 560+30]
 		
-
-
-
-		# move_down 10
-
-		# text "Ward officer", size: 16, style: :bold, color: '818a91'
-		# text "#{@ward.ward_officer} #{@ward.ward_officer_number}"
-		# move_down 10
-
-		# text "Ward officer", size: 16, style: :bold, color: '818a91'
-		# text "#{@ward.ward_officer} #{@ward.ward_officer_number}"
-		# move_down 10
-
-		# text "Ward officer", size: 16, style: :bold, color: '818a91'
-		# text "#{@ward.ward_officer} #{@ward.ward_officer_number}"
-		# move_down 10
 	end
-
-
-	# def	bounding_box([0, cursor-50], :width => 200, :height => 150)
-	#  transparent(0.5) { stroke_bounds }
-	#  text "This text will flow along this bounding box we created for it. " * 5
-	# end
-
-	# def bounding_box([300, y_position], :width => 200, :height => 150)
-	#  transparent(0.5) { stroke_bounds } # This will stroke on one page
-	#  text "Now look what happens when the free flowing text reaches the end " +
-	#  "of a bounding box that is narrower than the margin box." +
-	#  " . " * 200 +
-	#  "It continues on the next page as if the previous bounding box " +
-	#  "was cloned. If we want it to have the same border as the one on " +
-	#  "the previous page we will need to stroke the boundaries again."
-	#  transparent(0.5) { stroke_bounds } # And this will stroke on the next
-	# end
 
 	def labour_strength
 		move_down 180
@@ -87,11 +66,9 @@ class WardPdf < Prawn::Document
 	end
 
 	def footer
-		canvas do
-			text_box "B-14, MIDC Railway Station, Aurangabad - 431 001, INDIA, " + 
-			"Phone: +91 80074 47750, Email: info@civicresponseteam.com", 
-			:at => [65, 30], size: 8
-		end
+		text_box "<b>© #{Date.today.year} Civic Response Team</b>\n<color rgb='818a91'>B-14, MIDC Railway Station, Aurangabad - 431 001, India, " + 
+		"Phone: +91 80074 47750, Email: info@civicresponseteam.com</color>", 
+		valign: :bottom, align: :center, size: 8, inline_format: :true
 	end
 
 end
