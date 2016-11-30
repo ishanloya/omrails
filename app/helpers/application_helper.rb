cbb43568c766380977956e6789148183fb61da75
Ishan Loya <ishanloya@gmail.com>
2016-11-30 14:40:17 +0530

    Add helper to change active nav item status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def nav_link(link_text, link_path)
+		class_name = current_page?(link_path) ? 'nav-item active' : 'nav-item'
+
+		content_tag(:li, :class => class_name) do
+			link_to link_text, link_path, class: "nav-link"
+		end
+	end
 end
